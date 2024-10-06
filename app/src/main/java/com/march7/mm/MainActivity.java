9bfe6ad56200d9195e2e8d3ab91339dd6e306169
Erua <93416761+GilgameshSAMA@users.noreply.github.com>
2024-10-06 17:55:22 +0800

    Changes

---
+++ app/src/main/java/com/march7/mm/MainActivity.java
@@ -0,0 +1,202 @@
+package com.march7.mm;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    private ProgressBar spinner;
+    private String myurl = "https://march7.online"; // Change this to your own url !!
+    private ValueCallback<Uri[]> filePathCallback;
+    private static final int FILECHOOSER_RESULTCODE = 1;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        spinner = findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new CustomWebViewClient());
+
+        // 设置通知栏颜色为半透明黑色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(Color.argb(128, 0, 0, 0)); // 设置为半透明黑色
+        }
+
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setAllowFileAccess(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webview.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(myurl);
+
+        // 设置WebChromeClient以支持文件选择器
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(Intent.createChooser(intent, "File Chooser"), FILECHOOSER_RESULTCODE);
+
+                return true;
+            }
+        });
+
+        // 添加JavaScript接口
+        webview.addJavascriptInterface(new WebAppInterface(), "Android");
+    }
+
+    private class WebAppInterface {
+        @JavascriptInterface
+        public void setNightMode() {
+            runOnUiThread(() -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(Color.BLACK); // 设置通知栏为黑色
+                }
+            });
+        }
+    }
+
+
+    // 处理文件选择器的结果
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (filePathCallback != null) {
+                Uri[] results = null;
+                if (resultCode == RESULT_OK) {
+                    if (data != null) {
+                        String dataString = data.getDataString();
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+                filePathCallback.onReceiveValue(results);
+                filePathCallback = null;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        // 处理SSL问题
+        @Override
+        public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(R.string.notification_error_ssl_cert_invalid);
+            builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.proceed();
+                }
+            });
+
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    handler.cancel();
+                }
+            });
+
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+            webview.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            spinner.setVisibility(View.GONE);
+            view.setVisibility(View.VISIBLE);
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            setContentView(R.layout.error);
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webview.canGoBack()) {
+                        webview.goBack();
+                    } else {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage(R.string.exit_app);
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do nothing
+                            }
+                        });
+
+                        final AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public void tryAgain(View v) {
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webView);
+        spinner = findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new CustomWebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setAllowFileAccess(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(myurl);
+    }
+}
